a050b9a8f393a5e0362bdb2e55de36f385b2cf35
grueni95 <fg74bifo@studserv.uni-leipzig.de>
2022-06-16 12:05:15 +0200

    first steps in encryption

--- Cryptonize/encrypt.java
+++ Cryptonize/encrypt.java
@@ -9,4 +9,36 @@ package Cryptonize;
 public class encrypt {
 
     /* here is where the encryption happens and where I lost my hope in my english skills*/
+
+    private int key = 3;
+
+    private String inputString;
+
+    public encrypt(int key, String inputString) {
+        this.key = key;
+        this.inputString = inputString;
+        startEncryption(key, inputString);
+    }
+
+    public void startEncryption(int key, String inputString) {
+        /* for i in string mache i = (i + key) mod 26*/
+        String chiffre = "";
+        for (int i = 0; i < inputString.length(); i++) {
+
+            char a = inputString.charAt(i);
+            //System.out.println(a + " at " + (a+ key) % 26);
+            char b = inputString.charAt((i + key) % 26);
+            chiffre = chiffre + b;
+
+            //hier müsste das alphabet noch mit ins spiel damit man das ordentlich verschiebene kann.
+            /*System.out.println(a + " at " + i);
+            i = (i + key) % 26; // muss in den output string, sonst loooopts ewig
+            System.out.println("und danach " + a + " at " + i);*/
+        }
+        System.out.println(chiffre);
+    }
+/*
+    public static char shift(char a, int key) {
+
+    }*/
 }

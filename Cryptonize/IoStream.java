05c35c4e4941e3454ae28b527108d2e9ffadb2fb
jc39doja <jc39doja@studserv.uni-leipzig.de>
2022-06-18 19:03:28 +0200

    muss nur die special characters funktion aendern

--- Cryptonize/IoStream.java
+++ Cryptonize/IoStream.java
@@ -14,7 +14,6 @@ public class IoStream {
 
     private String ipath;
     private String opath;
-    private String schars;
     private int character;
 
     public IoStream(){
@@ -22,49 +21,57 @@ public class IoStream {
         this.opath = opath;
     }
 
-    public void readFile() throws IOException {
-
-        File file1 = new File ("Daten/3/Gedicht.txt");
-        File file2 = new File ("Daten/3/Thcideg.txt");
-        BufferedReader ipath = (new BufferedReader(new FileReader(file1)));
-        PrintWriter opath = (new PrintWriter(new FileWriter(file2)));
-        StringBuilder text = new StringBuilder();
-        String line = ipath.readLine();
-        while(line != null){
-            text.append(line);
-            text.append("\n");
-            line = ipath.readLine();
-        }
-        String string = text.toString();
-        replaceSpecialCharacters(schars);//modify replaceSpecialCharacters
-        text = new StringBuilder(string.toLowerCase());
-
-        while(text.length()>0){
-            if(text.indexOf("\n") >= 0){
-                opath.println(text.substring(0,text.indexOf("\n")));
-                text.delete(0,text.indexOf("\n") +1);
-            }
-            else{
-                opath.println(text);
-                text.delete(0,text.length());
+    public void readFile(){
+        try {
+            File file1 = new File("Daten/3/Gedicht.txt");
+            File file2 = new File("Daten/3/Thcideg.txt");
+            BufferedReader ipath = (new BufferedReader(new FileReader(file1)));
+            PrintWriter opath = (new PrintWriter(new FileWriter(file2)));
+            StringBuilder text = new StringBuilder();
+            String line = ipath.readLine();
+            while (line != null) {
+                text.append(line);
+                text.append("\n");
+                line = ipath.readLine();
             }
 
+            String string = text.toString();
+            string = string.replace("ä", "ae").replace("ö", "oe")
+                    .replace("ü", "ue").replace("ß", "ss");
+
+            text = new StringBuilder(string.toLowerCase());
+            while (text.length() > 0) {
+                if (text.indexOf("\n") >= 0) {
+                    opath.println(text.substring(0, text.indexOf("\n")));
+                    text.delete(0, text.indexOf("\n") + 1);
+                } else {
+                    opath.println(text);
+                    text.delete(0, text.length());
+                }
+
+            }
+
+            ipath.close();
+            opath.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
         }
 
-        ipath.close();
-        opath.close();
 
-
-        }
+    }
 
 
 
-    public void replaceSpecialCharacters(String schars) throws IOException{
+    /*public void replaceSpecialCharacters(String schars) throws IOException{
 
         schars = schars.replace("ä", "ae").replace("ö", "oe")
                 .replace("ü", "ue").replace("ß", "ss");
 
-    }
+    }*/
 
     }
 
